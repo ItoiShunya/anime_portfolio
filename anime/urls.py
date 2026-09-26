d267e0443095b1fbe753e1454cbaa64bafb0160b
Shunya <241205013@ccmailg.meijo-u.ac.jp>
2026-09-26 09:05:03 +0900

    Update urls.py

--- anime/urls.py
+++ anime/urls.py
@@ -5,6 +5,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
+app_name = 'anime' 
+
 urlpatterns = [
     # トップページ
     path('', views.index, name='index'),
