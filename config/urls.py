e068f6de4349b16abc30e696838418e209742e45
Shunya <241205013@ccmailg.meijo-u.ac.jp>
2026-09-26 09:43:30 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('anime.urls'), namespace='anime'),
+    path('', include('anime.urls', namespace='anime')),
 ]
